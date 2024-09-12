30b450e6945b13ebdb672ec90f3d2888a847fad3
Aditya Shinde <shindeaditya685@gmail.com>
2024-09-12 19:35:54 +0530

    done changes in next config file for deployement

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["img.clerk.com"],
+  },
+};
 
 export default nextConfig;
